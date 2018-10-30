00ec8ea2ad62e46184d889192bc567f4090f2f4d
JingkaiZhao <jingkai.zhao@foxmail.com>
2018-10-30 11:41:11 +0800

    feat: only use multi-compiler mode in production build

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = (api, options) => {
+  if (process.env.NODE_ENV !== 'production') return
+  // multi-compiler mode only can be used in production build
   const merge = require('webpack-merge')
   const oldResolveWebpackConfig = api.service.resolveWebpackConfig
 
   api.service.resolveWebpackConfig = function resolveWebpackConfig () {
     // get raw config
     let config = oldResolveWebpackConfig.call(this)
-    const { configureMultiCompilerWebpack } = options.pluginOptions
+    const { configureMultiCompilerWebpack } = options.pluginOptions || {}
 
     // tweak raw config into multi-compiler mode configs
     if (typeof configureMultiCompilerWebpack === 'function') {
